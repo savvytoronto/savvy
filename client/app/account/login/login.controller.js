e0cceae119be390b34259833c195ede8d03722f0
HarryHH <harryh@esna.com>
2015-09-23 21:22:04 -0400

    some ui

--- client/app/account/login/login.controller.js
+++ client/app/account/login/login.controller.js
@@ -1,14 +1,15 @@
 'use strict';
 
 angular.module('savvyAppApp')
-  .controller('LoginCtrl', function ($scope, Auth, $location) {
+  .controller('LoginCtrl', function ($scope, Auth, $timeout, $http, $location) {
+
     $scope.user = {};
     $scope.errors = {};
 
     $scope.login = function(form) {
       $scope.submitted = true;
 
-      if(form.$valid) {
+      if(validUser($scope.user)) {
         Auth.login({
           email: $scope.user.email,
           password: $scope.user.password
@@ -22,5 +23,29 @@ angular.module('savvyAppApp')
         });
       }
     };
+
+    $scope.validEmail = function (email) {
+      var valid = false;
+      if (email.indexOf('@') > 0) {
+        valid = true;
+      }
+      return valid;
+    };
+
+    var validUser = function (user) {
+      return true;
+    };
+
+    $scope.checkUser = function (email) {
+
+      $scope.loadUser = 'load';
+      //api call here
+
+      //when successful
+      $timeout(function () {
+        $scope.loadUser = 'success';
+      }, 3000);
+    };
+
 
   });
--- client/app/main/main.js
+++ client/app/main/main.js
@@ -1,11 +1,15 @@
 'use strict';
 
 angular.module('savvyAppApp')
-  .config(function ($stateProvider) {
-    $stateProvider
-      .state('main', {
-        url: '/',
-        templateUrl: 'app/main/main.html',
-        controller: 'MainCtrl'
-      });
-  });
+.config(function ($stateProvider) {
+	$stateProvider
+		.state('template', {
+			url: '/template',
+			templateUrl: 'components/theme/html/index.html'
+		})
+	  	.state('main', {
+		    url: '/',
+		    templateUrl: 'app/main/main.html',
+	  	  	controller: 'MainCtrl'
+	  	});
+});	
