1db419131168b8e86a6ee1e1dcbb9d5e0255e808
Charan <charan.guru90@gmail.com>
2015-09-25 03:05:34 -0400

    Forgot to commit newest changes to seed.js

--- server/config/seed.js
+++ server/config/seed.js
@@ -6,6 +6,7 @@
 'use strict';
 
 var Campaign = require('../api/campaign/campaign.model');
+var Product = require('../api/campaign/product.model');
 var User = require('../api/user/user.model');
 var Store = require('../api/store/store.model');
 var Coupon = require('../api/coupon/coupon.model');
@@ -111,8 +112,47 @@ var generateCampaign = function (cb) {
 
         }
         newCampaign = this_campaign;
+        //generateProducts(cb());
         return cb();
       })
     });
 });
+}
+
+//Where should I call this?
+var generateProducts = function (cb) {
+  Product.find({}).remove(function() {
+    Product.create(
+      {name:"Slim-Fit Non-Iron Dark Chambray Shirt",price:74.97,code:"257240",image_url:"assets/images/257240.jpg",qty:0,store_id:newStore._id},
+      {name:"Slim-Fit Custom 078 Wash Dot Shirt",price:61.99,code:"722313",image_url:"assets/images/722313.jpg",qty:24,store_id:newStore._id},
+      {name:"Tailored Slim-Fit Non-Iron Micro-Check Shirt",price:87.99,code:"675134",image_url:"assets/images/675134.jpg",qty:22,store_id:newStore._id},
+      {name:"Slim-Fit Custom 078 Wash Heathered Shirt (M)",price:61.99,code:"722300",image_url:"assets/images/722300.jpg",qty:13,store_id:newStore._id},
+      {name:"Vintage Straight Indigo Jean",price:93.99,code:"266016",image_url:"assets/images/266016.jpg",qty:12,store_id:newStore._id},
+      {name:"Modern Slim Textured Gray Suit Trouser (M)",price:141.99,code:"186127",image_url:"assets/images/186127.jpg",qty:13,store_id:newStore._id},
+      {name:"Modern Slim Navy Linen Suit Trouser",price:117.99,code:"402029",image_url:"assets/images/402029.jpg",qty:23,store_id:newStore._id},
+      {name:"Aiden Slim Pinstripe Linen Short",price:54.99,code:"404572",image_url:"assets/images/404572.jpg",qty:11,store_id:newStore._id},
+      {name:"Aiden Slim Garment-Dye Utility Short",price:58.99,code:"402087",image_url:"assets/images/402087.jpg",qty:7,store_id:newStore._id},
+      {name:"Aiden Slim Textured Cotton Short",price:66.99,code:"289069",image_url:"assets/images/289069.jpg",qty:5,store_id:newStore._id},
+      {name:"NYC/SF",price:87.99,code:"674187",image_url:"assets/images/674187.jpg",qty:4,store_id:newStore._id},
+      {name:"Military Vee Pullover",price:67.99,code:"674084",image_url:"assets/images/674084.jpg",qty:2,store_id:newStore._id},
+      {name:"Chest-Pocket Crew Pullover",price:85.99,code:"673222",image_url:"assets/images/673222.jpg",qty:2,store_id:newStore._id},
+      {name:"Modern Slim Navy Linen Suit Jacket",price:277.99,code:"402028",image_url:"assets/images/402028.jpg",qty:0,store_id:newStore._id},
+      {name:"Monogram Blue Micro-Check Italian Wool Suit Jacket (M)",price:439.99,code:"186118",image_url:"assets/images/186118.jpg",qty:3,store_id:newStore._id},
+      {name:"Tailored-Fit Grey Micro-Dot Wool Suit Jacket (M)",price:303.99,code:"969653",image_url:"assets/images/969653.jpg",qty:4,store_id:newStore._id},
+      {name:"Four-Pocket Shirt Jacket",price:137.99,code:"404250",image_url:"assets/images/404250.jpg",qty:5,store_id:newStore._id},
+      {name:"Modern Slim Blue Linen Blazer",price:171.97,code:"257234",image_url:"assets/images/257234.jpg",qty:0,store_id:newStore._id},
+      {name:"Micro-Stripe Silk Tie",price:58.99,code:"403634",image_url:"assets/images/403634.jpg",qty:8,store_id:newStore._id},
+      {name:"Micro-Print Silk Tie",price:62.99,code:"403635",image_url:"assets/images/403635.jpg",qty:0,store_id:newStore._id},
+      {name:"Sloan-Fit Faux-Leather Trim Ankle Pant",price:124.00,code:"673130",image_url:"assets/images/673130.jpg",qty:2,store_id:newStore._id},
+      {name:"Silk Banded V-Neck Blouse",price:77.00,code:"676053",image_url:"assets/images/676053.jpg",qty:4,store_id:newStore._id}, function () {
+        Product.findOne({
+        code:"257240"
+      }, function(err) {
+        if(err) {
+
+        }
+        return cb();
+      })
+    });
+  });
 }
