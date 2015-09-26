f7f3e9c5958fa47f4a3b15d9aa3a7312283e03d7
Eric <ericfang@mavencare.com>
2015-09-26 01:16:14 -0400

    fix ui

--- client/app/main/collection/collection.controller.js
+++ client/app/main/collection/collection.controller.js
@@ -2,21 +2,21 @@
 
 angular.module('savvyAppApp')
   .controller('CollectionCtrl', function ($scope, $http) {
-    $scope.awesomeThings = [];
+    // $scope.awesomeThings = [];
 
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
+    // $http.get('/api/things').success(function(awesomeThings) {
+    //   $scope.awesomeThings = awesomeThings;
+    // });
 
-    $scope.addThing = function() {
-      if($scope.newThing === '') {
-        return;
-      }
-      $http.post('/api/things', { name: $scope.newThing });
-      $scope.newThing = '';
-    };
+    // $scope.addThing = function() {
+    //   if($scope.newThing === '') {
+    //     return;
+    //   }
+    //   $http.post('/api/things', { name: $scope.newThing });
+    //   $scope.newThing = '';
+    // };
 
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
-    };
+    // $scope.deleteThing = function(thing) {
+    //   $http.delete('/api/things/' + thing._id);
+    // };
   });
--- client/app/main/inventory/inventory.controller.js
+++ client/app/main/inventory/inventory.controller.js
@@ -1,22 +1,22 @@
 'use strict';
 
 angular.module('savvyAppApp')
-  .controller('InventoryCtrl', function ($scope, Auth, $http) {
+  .controller('InventoryCtrl', function ($scope, $http) {
     $scope.awesomeThings = [];
 
-    $http.get('/api/products').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
+    // $http.get('/api/products').success(function(awesomeThings) {
+    //   $scope.awesomeThings = awesomeThings;
+    // });
 
-    $scope.addThing = function() {
-      if($scope.newThing === '') {
-        return;
-      }
-      $http.post('/api/things', { name: $scope.newThing });
-      $scope.newThing = '';
-    };
+    // $scope.addThing = function() {
+    //   if($scope.newThing === '') {
+    //     return;
+    //   }
+    //   $http.post('/api/things', { name: $scope.newThing });
+    //   $scope.newThing = '';
+    // };
 
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
-    };
+    // $scope.deleteThing = function(thing) {
+    //   $http.delete('/api/things/' + thing._id);
+    // };
   });
